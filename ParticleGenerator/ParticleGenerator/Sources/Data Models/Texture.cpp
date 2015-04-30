dbf4e042b249a4f5a724bea1a4872e24c6f86e09
SaranHasnor <SaranHasnor@users.noreply.github.com>
2015-04-30 23:37:46 +0200

    Debuggage de la texture

--- ParticleGenerator/ParticleGenerator/Sources/Data Models/Texture.cpp
+++ ParticleGenerator/ParticleGenerator/Sources/Data Models/Texture.cpp
@@ -8,13 +8,6 @@ Texture::Texture(std::string path)
 	this->path = path;
 
 	this->data = stbi_loadf(path.c_str(), &this->width, &this->height, NULL, 4);
-	for (int i = 0; i < this->width * this->height * 4; i += 4)
-	{
-		((float*)this->data)[i+0] = (float)((i / 4) % (this->width * 4));
-		((float*)this->data)[i+1] = (float)((i / 4) % (this->width * 4));
-		((float*)this->data)[i+2] = (float)((i / 4) % (this->width * 4));
-		((float*)this->data)[i+3] = 1.0f;
-	}
 
 	this->textureID = 0; // TEMP
 }
